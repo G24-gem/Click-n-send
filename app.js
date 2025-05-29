e9461fd45a27f5ec8ab26bf2fdd30cd334920dd1
G24-gem <127473139+G24-gem@users.noreply.github.com>
2025-05-29 01:54:48 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,46 @@
+const WebSocket = require("ws");
+const fs = require("fs")
+const server = new WebSocket.Server({port: 5555})
+
+if(!fs.existsSync("./chathouse")){
+   fs.mkdirSync("./chathouse")
+}
+
+server.on("connection", (ws) => {
+	ws.on("message", (message) => {
+		var msg = message.toString("utf8");
+		var content = msg.split(",")[0];
+		var filename = msg.split(",")[1];
+        
+        fs.writeFile(`./chathouse/${filename}`, content, (err) => {
+           if(err) console.log(err)
+        })
+       
+       
+
+		console.log(message.toString("utf8"), content) 
+	});
+     async function send() {
+      var con = await fs.readdir("./chathouse", (err, files) => {
+          const filesArr = files;
+          filesCon = [];
+          filesArr.forEach((file) => {
+            filesCon.push(fs.readFileSync(`./chathouse/${file}`, "utf8"))
+          })
+          console.log(filesCon)
+       
+          function timed() {
+          
+          	ws.send(filesCon.join("|"))
+
+      }
+      timed()
+      });
+       	
+     }
+     
+     send();
+     setInterval(send, 10)
+
+
+})
---
+++ script.js
@@ -0,0 +1,71 @@
+const inputElem = document.querySelector(".input");
+const submitElem = document.querySelector(".post-btn");
+const chatCanvas = document.querySelector(".post-container");
+const ws = new WebSocket("ws://localhost:5555");
+var timestamp = new Date().toISOString().replace(/:/g, "-");
+var msgArr = [];
+
+function delFun(Arr, index) {
+	const ind = index;
+	var Arr = Arr;
+    
+	for (var i = index; i < Arr.length-1; i++) {
+		Arr[i] = Arr[i+1];
+	}
+	if (!(Arr.length == 0)) {Arr.length--};
+	return Arr;
+}
+
+
+
+
+//listens for server's message
+ws.onmessage = (message) => {
+	chatCanvas.innerHTML = "";
+	var msgArrLocal  = message.data.split("|");
+	 msgArrLocal.forEach( (item) => {
+		var div = document.createElement("div");
+        div.id = "mess";
+        div.innerHTML = item;
+        chatCanvas.appendChild(div);
+      })
+}
+ chatCanvas.scrollTop = chatCanvas.scrollHeight;
+
+//sends the receive message
+function sendMessageEvent() {
+	inputElem.addEventListener("keydown", (e) => {
+	     var msg = "";
+	     function sendMessage() {
+	     	if(e.key.length == 1) {
+			      msgArr.push(e.key);
+			      msg = [`${msgArr.join().replace(/,/g, "")}`, timestamp];
+			      ws.send(msg);  
+	           }
+	     }
+
+
+	  var specialKeys = "backspace" || "enter";
+	  var keyLower = e.key.toLowerCase();
+	    
+	     sendMessage();
+	     
+	    if (keyLower == specialKeys) {
+	    	if (specialKeys == "backspace") { 
+	    		 msgArr = delFun(msgArr, msgArr.length)
+                 var msg1 = [`${msgArr.join().replace(/,/g, "")}`, timestamp];
+	    		 console.log(msgArr)
+	             ws.send(msg1)
+	    	}
+	    }
+	   
+	})
+}
+
+sendMessageEvent()
+submitElem.addEventListener("click", () => {
+   inputElem.value = "";
+   timestamp = new Date().toISOString().replace(/:/g, "-");
+}) 
+
+
