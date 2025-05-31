0ebb6e3ecebb9f4aa5a1d873c26d83bd4fe5b2a3
G24-gem <127473139+G24-gem@users.noreply.github.com>
2025-05-31 18:03:13 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const app = express();
 const fs = require("fs")
 const server = new WebSocket.Server({port: 5555})
 
-const express = require('express');
+
 const path = require('path');
-const app = express();
 
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
